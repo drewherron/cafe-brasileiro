fc5438adbab2425bb135b0a6ee3f59e71d3ead99
Drew Herron <herron@mailbox.org>
2024-06-05 17:23:51 -0700

    Files load to dataframes

--- preco_do_cafe.py
+++ preco_do_cafe.py
@@ -1,22 +1,47 @@
+import os
 import numpy as np
 import pandas as pd
 
+
+data_dir = "data"
+
 def load_coffee_data(filename):
-    # TODO
+    """Load the coffee price data from CSV"""
+    coffee_df = pd.read_csv(filename,
+                            decimal=",",
+                            thousands=".",
+                            parse_dates=['Data'],
+                            dayfirst=True)
     return coffee_df
 
 def load_weather_data(filename):
-    # TODO
-    return weather_df
+    """Load the daily weather data from CSV"""
+    daily_weather_df = pd.read_csv(filename,
+                           sep=";",
+                           decimal=",",
+                           thousands=".",
+                           skiprows=10,
+                           parse_dates=['Data Medicao'],
+                           dayfirst=True)
+    daily_weather_df.columns = daily_weather_df.columns.str.strip()
+
+    return daily_weather_df
 
 def main():
 
-    # Load data
-    coffee_df = load_coffee_data('cafe_arabica_weekly.csv')
+    # Load coffee data
+    coffee_file = "cafe_arabica_weekly.csv"
+    coffee_path= os.path.join(data_dir, coffee_file)
+    coffee_df = load_coffee_data(coffee_path)
+    print(coffee_df.head())
 
-    # Combine the datasets on the date (Data) column
-    combined_df = pd.merge(coffee_df, weather_df, on='Data', how='inner')
-    # Probably
+    # Load weather data
+    weather_file = "dados_83393_D_1970-01-01_2024-05-14.csv"
+    weather_path= os.path.join(data_dir, "daily_weather", weather_file)
+    weather_df = load_weather_data(weather_path)
+    print(weather_df.head())
+
+
 
 
 if __name__ == "__main__":
